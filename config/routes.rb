844c688b811dd16141ad22237ba664014f29b980
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-11-05 13:46:48 +0000

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+# ユーザー用
+devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+
   root "homes#top"
   get "about" => "homes#about"
 end
