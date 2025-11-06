c09c2faca248df9b9a91b6db577e7e39f6fce749
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-11-06 13:07:12 +0000

    ユーザー編集機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,10 +7,24 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to mypage_path
+  end
+
 
   def mypage
     @user = current_user
     @posts = @user.posts.order(created_at: :desc).page(params[:page])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
