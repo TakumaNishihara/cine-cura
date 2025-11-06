1dba1c52b46bac1700c424d6dc28814920891f46
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-11-06 08:54:20 +0000

    画像がない時の対処

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,13 @@ class Post < ApplicationRecord
     only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99,
     allow_nil: true, message: 'は1〜99の数字で入力してください'
   }
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+  
 end
