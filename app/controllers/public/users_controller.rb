286720bb29f10d31c3f6a33c0eae53618acb5b15
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-11-06 12:28:05 +0000

    mypage作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:id])
@@ -8,5 +10,7 @@ class Public::UsersController < ApplicationController
   end
 
   def mypage
+    @user = current_user
+    @posts = @user.posts.order(created_at: :desc).page(params[:page])
   end
 end
