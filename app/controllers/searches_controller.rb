59a81e375a66634f416fd1ef89b3b048b3ff4c99
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-11-13 08:20:04 +0000

    タグ検索修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,16 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
-
 	def search
 		@model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
 
 		if @model == 'user'
-      @records = User.search_for(@content, @method)
+			@records = User.search_for(@content, @method)
 		elsif @model == "post_tag"
-      @records = Post.search_for_tag(@content, @method)
+			@records = Post.search_for_tag(@content, @method)
 		else
-      @records = Post.search_for_title(@content, @method)
-	  end
+			@records = Post.search_for_title(@content, @method)
+		end
   end
 end
