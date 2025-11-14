ca10d88082213060206b9498e43e6822dc7f1e64
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-11-14 04:32:55 +0000

    投稿詳細編集

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,7 +1,10 @@
 class Public::PostsController < ApplicationController
   
-  # 投稿は未ログインでも見れるように
-  before_action :authenticate_user!, except: [:index, :show]
+  # 投稿の閲覧は未ログインでも可
+  before_action :authenticate_user_or_admin!, except: [:index, :show]
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_post_owner_or_admin!, only: [:edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -52,6 +55,23 @@ class Public::PostsController < ApplicationController
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # 「ユーザー or 管理者」のいずれかがログインしていればOK
+  def authenticate_user_or_admin!
+    return if user_signed_in? || admin_signed_in?
+    redirect_to new_user_session_path, alert: 'ログインしてください'
+  end
+
+  # 「投稿者本人 or 管理者」以外は拒否
+  def authorize_post_owner_or_admin!
+    return if admin_signed_in? || (user_signed_in? && @post.user_id == current_user.id)
+    redirect_to post_path(@post), alert: '権限がありません'
+  end
+
   def post_params
     params.require(:post).permit(:title, :body, :release, :timeline, :tag, :image)
   end
